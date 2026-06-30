86a547ca29c6f7c1b0c70d4696968f5311a7c8bd
Andrey (AMG) <gggamerdosgames@gmail.com>
2026-06-29 21:27:50 -0400

    fixes and update setup

--- setup.js
+++ setup.js
@@ -13,8 +13,10 @@ execSync("npm install", { stdio: "inherit" });
 console.log("Generating Prisma Client...");
 execSync("npx prisma generate", { stdio: "inherit" });
 
+const migrationName = `init_${Date.now()}`;
+
 console.log("Running migrations...");
-execSync("npx prisma migrate dev", { stdio: "inherit" });
+execSync(`npx prisma migrate dev --name ${migrationName}`, { stdio: "inherit" });
 
 console.log("Seeding database...");
 execSync("npm run seed", { stdio: "inherit" });
